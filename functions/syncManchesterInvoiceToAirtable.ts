a6e7fc1647155c03e337923b40b2b866d014b526
Steve Brown <steve.brown@enticmd.com>
2025-12-05 04:00:40 +0000

    File changes

--- functions/syncManchesterInvoiceToAirtable.ts
+++ functions/syncManchesterInvoiceToAirtable.ts
@@ -67,9 +67,9 @@ Deno.serve(async (req) => {
 
     // Construct Email Content
     const emailSubject = `Manchester ${invoiceMonth} Invoices`;
-    const toRecipient = "apacileo@echn.org";
+    const toRecipient = "brownsteven89@gmail.com, brownsteven89@icloud.com";
     // Ensure these are the correct CCs
-    const ccRecipients = "steve.brown@enticmd.com, heldridge@enticmd.com";
+    const ccRecipients = "steve.brown@enticmd.com";
     
     const emailBody = `Hey Team,\n\nHope your week is off to a fantastic start.\n\nThe ${invoiceMonth} clinic session details for you to process and enter for:\n\n${providerList}\n\nPlease see the attached invoices.\n\nThank you so much,\nSteve Brown\nOperations Manager`;
 
