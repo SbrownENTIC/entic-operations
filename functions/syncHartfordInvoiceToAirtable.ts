efd19bb93c57ecfa3024562da675170f14d1a0e4
Steve Brown <steve.brown@enticmd.com>
2025-12-09 15:16:40 +0000

    File changes

--- functions/syncHartfordInvoiceToAirtable.ts
+++ functions/syncHartfordInvoiceToAirtable.ts
@@ -76,7 +76,7 @@ Deno.serve(async (req) => {
     const invoiceNumbers = validInvoices.map(i => i.invoice_number).join(', ') || "(No Invoice Numbers)";
 
     const emailSubject = `Hartford Hospital ${invoiceMonth} Invoices`;
-    const toRecipient = "apacileo@echn.org";
+    const toRecipient = "ap@hhchealth.org";
     const ccRecipients = "steve.brown@enticmd.com";
     
     const emailBody = `Hey Team,\n\nHope your week is off to a fantastic start.\n\nThe Hartford Hospital ${invoiceMonth} clinic session details for you to process and enter for:\n\n${providerList}\n\nPlease see the attached invoices.\n\nThank you so much,\nSteve Brown\nOperations Manager`;
