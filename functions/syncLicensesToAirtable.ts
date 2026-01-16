1cdb9f4e7aa39ffeca11de6b463238ad005c07d8
Steve Brown <steve.brown@enticmd.com>
2026-01-16 20:51:42 +0000

    File changes

--- functions/syncLicensesToAirtable.ts
+++ functions/syncLicensesToAirtable.ts
@@ -151,7 +151,7 @@ Deno.serve(async (req) => {
         'License Type': cleanLicenseType,
         'Expiration Date': license.expiration_date || null,
         'Status': license.status || 'active',
-        // 'Internal License ID': internalId || '', // Cannot write to computed field
+        'Internal License ID': internalId || '',
         'Reminder 30 Days': license.reminder_30_sent || false,
         'Reminder 14 Days': license.reminder_14_sent || false,
         'Reminder 7 Days': license.reminder_7_sent || false
