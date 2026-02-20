8e7ee1466aff7db1d31cba3ddbd2dca34789826c
Steve Brown <steve.brown@enticmd.com>
2026-02-20 16:37:38 +0000

    File changes

--- functions/syncInvoiceBalances.ts
+++ functions/syncInvoiceBalances.ts
@@ -1,7 +1,9 @@
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.4';
+import { logSystemEvent } from "./utils/systemLogger.js";
 
 Deno.serve(async (req) => {
   try {
+    await logSystemEvent("syncInvoiceBalances", "START");
     const base44 = createClientFromRequest(req);
     
     // Verify user is authenticated
