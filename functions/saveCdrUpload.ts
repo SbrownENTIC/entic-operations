1c21f759027db99626ec7d845c9d2a7c46340084
Steve Brown <steve.brown@enticmd.com>
2026-02-27 19:41:05 +0000

    File changes

--- functions/saveCdrUpload.ts
+++ functions/saveCdrUpload.ts
@@ -92,7 +92,7 @@ Deno.serve(async (req) => {
         total_unanswered,
         mapped_rows,
         unmapped_rows,
-        unmapped_extensions: unmapped_extensions || []
+        unmapped_extensions: Array.isArray(unmapped_extensions) ? unmapped_extensions : []
       });
     } else {
       // Create new
