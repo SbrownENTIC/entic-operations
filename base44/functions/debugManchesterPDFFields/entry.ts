b9394b45037d5f4f6a2c9599197f4477218b18d9
Steve Brown <steve.brown@enticmd.com>
2026-04-09 18:58:53 +0000

    File changes

--- base44/functions/debugManchesterPDFFields/entry.ts
+++ base44/functions/debugManchesterPDFFields/entry.ts
@@ -2,7 +2,7 @@ import { PDFDocument } from 'npm:pdf-lib@1.17.1';
 
 Deno.serve(async (req) => {
     try {
-        const templateUrl = "https://media.base44.com/files/public/691521cbabed77e5043c7037/23b640b06_TestHH-Manchester-ECHNInvoicetemplate-ENTIC.pdf";
+        const templateUrl = "https://media.base44.com/files/public/691521cbabed77e5043c7037/f85160c21_TestHH-Manchester-ECHNInvoicetemplate-ENTIC.pdf";"https://media.base44.com/files/public/691521cbabed77e5043c7037/23b640b06_TestHH-Manchester-ECHNInvoicetemplate-ENTIC.pdf";
         
         const templateResponse = await fetch(templateUrl);
         if (!templateResponse.ok) {
